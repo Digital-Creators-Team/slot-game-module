78c856a38b63a6dae308e6c95e34e6dc85b820a0
Thien <thien@Thiens-MacBook-Pro.local>
2026-04-09 14:20:50 +0700

    fix duplicate total win free spin

--- server/game_service.go
+++ server/game_service.go
@@ -343,7 +343,7 @@ func (s *GameService) executeNormalSpin(
 		if spinResult.ResultFreeSpin != nil && *spinResult.ResultFreeSpin > 0 {
 			playerState.IsFreeSpin = true
 			playerState.RemainingFreeSpin = *spinResult.ResultFreeSpin
-			playerState.TotalWinFreeSpin = &spinResult.TotalWin
+			playerState.TotalWinFreeSpin = lo.ToPtr(spinResult.TotalWin)
 			playerState.SpinResultTriggerFG = spinResult
 			playedCount := 0
 			playerState.PlayedFreeSpin = &playedCount
